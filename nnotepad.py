602149f754dd584d773a80207a4bc62ce9c7cc4d
sidharthmitra <118099310+sidharthmitra@users.noreply.github.com>
2023-03-15 21:19:19 +0530

    Update nnotepad.py

--- nnotepad.py
+++ nnotepad.py
@@ -34,7 +34,7 @@ def saveFile():
             file = None
 
         else:
-            #Save as a new file
+           
             f = open(file, "w")
             f.write(TextArea.get(1.0, END))
             f.close()
@@ -42,7 +42,7 @@ def saveFile():
             root.title(os.path.basename(file) + " - Notepad")
             print("File Saved")
     else:
-        # Save the file
+
         f = open(file, "w")
         f.write(TextArea.get(1.0, END))
         f.close()
@@ -69,45 +69,42 @@ def ran():
     root.title("Untitled - Notepad")
     root.geometry("644x788")
 
-    #Add TextArea
+   
     TextArea = Text(root, font="Algerian 17")
     file = None
     TextArea.pack(expand=True, fill=BOTH)
 
-    #menubar
+  
     MenuBar = Menu(root)
 
-    #File Menu Starts
+    
     FileMenu = Menu(MenuBar, tearoff=0)
-    # To open new file
+   
     FileMenu.add_command(label="New", command=newFile)
 
-    #To Open already existing file
+   
     FileMenu.add_command(label="Open", command = openFile)
 
-    # To save the current file
+   
 
     FileMenu.add_command(label = "Save", command = saveFile)
     FileMenu.add_separator()
     FileMenu.add_command(label = "Exit", command = quitApp)
     MenuBar.add_cascade(label = "File", menu=FileMenu)
-    # File Menu ends
-    # Edit Menu Starts
+ 
     EditMenu = Menu(MenuBar, tearoff=0)
-    #To give a feature of cut, copy and paste
+
     EditMenu.add_command(label = "Cut", command=cut)
     EditMenu.add_command(label = "Copy", command=copy)
     EditMenu.add_command(label = "Paste", command=paste)
     MenuBar.add_cascade(label="Edit", menu = EditMenu)
-    # Edit Menu Ends
-    # Help Menu Starts
+
     HelpMenu = Menu(MenuBar, tearoff=0)
     HelpMenu.add_command(label = "About Notepad", command=about)
     MenuBar.add_cascade(label="Help", menu=HelpMenu)
 
-    # Help Menu Ends
     root.config(menu=MenuBar)
-    #Adding Scrollbar using rules from Tkinter lecture no 22
+   
     Scroll = Scrollbar(TextArea)
     Scroll.pack(side=RIGHT,  fill=Y)
     Scroll.config(command=TextArea.yview)
