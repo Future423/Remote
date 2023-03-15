4a679d17d8d764a45b9f10dd7ffb92d5b1079183
Nishant <72150854+Future423@users.noreply.github.com>
2023-03-15 23:01:59 +0530

    Update remot1.py

--- remot1.py
+++ remot1.py
@@ -8,8 +8,8 @@ from tkinter.filedialog import*
 import time
 
 root = Tk()
-root.geometry("500x500")
-root.title('Remote')
+root.geometry("390x380")
+root.title('Python assist')
 root.config(bg="azure")
 root.resizable(0,0)
 
@@ -18,7 +18,8 @@ def INF():
 def INF2():
     messagebox.showinfo('Help','For FAQs or help contact harshitabroka2002@gmail.com/')
 def qit():
-    root.destroy()   
+    root.destroy()  
+    
 #menu-bar
 mnu= Menu(root)
 m1=Menu(mnu,tearoff=0)
@@ -28,8 +29,7 @@ m1.add_command(label='Exit',command=qit)
 mnu.add_cascade(label='Options',menu=m1)
 root.config(menu=mnu)
 
-#button......
-
+#button commands
 def hi():
     Weathern2.main()
 
@@ -46,13 +46,14 @@ def tk_ss():
     sp=asksaveasfilename()
     ss.save(sp+"_ss.png")
 
-b1=Button(root,text='Weather Report',font="caladea 22",bg="#fff",command=hi)
-b1.place(x=20,y=25)
-b2=Button(root,text='Notepad',font="caladea 22",bg="#fff",command=note)
-b2.place(x=200,y=80)
-b3=Button(root,text='Screen Shot',font="caladea 22",bg="#fff",command=tk_ss)
-b3.place(x=300,y=100)
-b4=Button(root,text='Calculator',font="caladea 22",bg="#fff",command=p2p)
-b4.place(x=300,y=25)
+#buttons
+b1=Button(root,text='Weather Report',font="Algerian 25",bg="#fff",command=hi)
+b1.place(x=35,y=25)
+b2=Button(root,text='Notepad',font="Algerian 25",bg="#fff",command=note)
+b2.place(x=100,y=250)
+b3=Button(root,text='Screen Shot',font="Algerian 25",bg="#fff",command=tk_ss)
+b3.place(x=72.5,y=100)
+b4=Button(root,text='Calculator',font="Algerian 25",bg="#fff",command=p2p)
+b4.place(x=75,y=175)
 
 root.mainloop()
